8cce48cce37232f4006740863510cd0626a7cb4e
msiwy12345 <mateuszsiwy00@gmail.com>
2024-09-06 20:56:48 +0200

    Added possibility of adding new stacks

--- Flashcards/CRUDoperations.cs
+++ Flashcards/CRUDoperations.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using DataModels;
 using System.Configuration;
+using Microsoft.Data.SqlClient;
+using Dapper;
 
 public class FlashcardsService
 {
@@ -14,4 +16,43 @@ public class FlashcardsService
         connectionString = ConfigurationManager.AppSettings.Get("connectionString");
 
     }
+
+    public List<string> GetStackNames()
+    {
+        try
+        {
+            using(var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string namesCommand = @"SELECT StackName FROM stacks";
+                List<string> stackNames = connection.Query<string>(namesCommand).ToList();
+                return stackNames;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.Write(ex.ToString());
+            return null;
+        }
+    }
+
+    public void CreateStack(string newStackName)
+    {
+        try
+        {
+            using(var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                
+                string createStackCommand = @"INSERT INTO stacks (StackName) VALUES(@StackName)";
+                var parameters = new { StackName = newStackName };
+                connection.Execute(createStackCommand, parameters);
+            }
+        }
+        catch(Exception ex)
+        {
+            Console.Write(ex.ToString());
+        }
+    }
+
 }
--- Flashcards/UserInterface.cs
+++ Flashcards/UserInterface.cs
@@ -8,6 +8,7 @@ using System.Configuration;
 using System.Collections.Specialized;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Protocols;
+
 namespace Flashcards
 {
     public class App
@@ -39,10 +40,10 @@ namespace Flashcards
                     
                     break;
                 case "1":
-
+                    ManageStacks();
                     break;
                 case "2":
-                    
+                    ManageFlashcards();
                     break;
                 case "3":
 
@@ -55,6 +56,48 @@ namespace Flashcards
                     ProcessMenuInput();
                     break;
             }
+        }
+
+        static public void ManageStacks()
+        {
+            Console.WriteLine("Input a stack name");
+            FlashcardsService service = new FlashcardsService();
+            List<string> stackNames = service.GetStackNames();
+            var table = new Table();
+            table.AddColumn("Stack name");
+            foreach (string stackName in stackNames)
+            {
+                table.AddRow(stackName);
+            }
+            AnsiConsole.Write(table);
+            Console.WriteLine("Press 0 - to create a new stack");
+            string option = Console.ReadLine();
+            if (option == "0")
+            {
+                Console.WriteLine("Please enter a new stack name");
+                string newStackName = Console.ReadLine();
+                while (stackNames.Contains(newStackName))
+                {
+                    Console.WriteLine("This name is already taken, enter a new one");
+                    newStackName = Console.ReadLine();
+                }
+                service.CreateStack(newStackName);
+            }
+            else if (stackNames.Contains(option))
+            {
+                ManageFlashcards();
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid stack name or 0 to create a new one");
+                
+            }
+            ManageStacks();
+        }
+
+        static public void ManageFlashcards()
+        {
+
         }
     }
 }
