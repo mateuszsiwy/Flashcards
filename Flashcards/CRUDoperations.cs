a661530858d3491065ec4b7a07d4403340ae6d30
msiwy12345 <mateuszsiwy00@gmail.com>
2024-09-07 12:09:49 +0200

    Added edit flashcard functionality

--- Flashcards/CRUDoperations.cs
+++ Flashcards/CRUDoperations.cs
@@ -55,4 +55,77 @@ public class FlashcardsService
         }
     }
 
+    public List<FlashcardDTO> GetFlashcards(int? amount, string currentStack)
+    {
+        try
+        {
+            using(var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                if (amount.HasValue)
+                {
+                    string getFlashcards = $@"SELECT TOP {amount} f.FlashcardId, f.Front, f.Back FROM flashcards f " +
+                       "INNER JOIN stacks s ON f.StackId = s.StackId WHERE s.StackName = @StackName";
+                    var parameters = new { StackName = currentStack };
+                    List<FlashcardDTO> flashcards = connection.Query<FlashcardDTO>(getFlashcards, parameters).ToList();
+                    return flashcards;
+                }
+                else
+                {
+                    string getFlashcards = @"SELECT f.FlashcardId, f.Front, f.Back FROM flashcards f " + 
+                       "INNER JOIN stacks s ON f.StackId = s.StackId WHERE s.StackName = @StackName";
+                    var parameters = new { StackName = currentStack };
+                    List<FlashcardDTO> flashcards = connection.Query<FlashcardDTO>(getFlashcards, parameters).ToList();
+                    return flashcards;
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.Write(ex.ToString());
+            return null;
+        }
+    }
+
+    public void AddFlashcard(string front, string back, string StackName)
+    {
+        try
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string getStackId = @"SELECT StackId from stacks WHERE StackName = @stackName";
+                int? id = connection.QuerySingleOrDefault<int?>(getStackId, new { StackName = StackName });
+
+                string insertFlashcard = @"INSERT into flashcards (Front, Back, StackId) VALUES(@Front, @Back, @StackId)";
+                var parameters = new { Front = front, Back = back, StackId = id };
+                connection.Execute(insertFlashcard, parameters);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.Write(ex.ToString());
+            
+        }
+    }
+
+    public void EditFlashcard(string front, string back, string id)
+    {
+        try
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+  
+                string updateFlashcard = @"UPDATE flashcards SET Front = @Front, Back = @Back WHERE FlashcardId = @FlashcardId ";
+                var parameters = new { Front = front, Back = back, FlashcardId = id };
+                connection.Execute(updateFlashcard, parameters);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.Write(ex.ToString());
+
+        }
+    }
+
 }
--- Flashcards/UserInterface.cs
+++ Flashcards/UserInterface.cs
@@ -1,103 +1,241 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using DataModels;
 using Spectre.Console;
-using System.Configuration;
-using System.Collections.Specialized;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Protocols;
 
 namespace Flashcards
 {
     public class App
     {
-        static public void StartApp()
+        private static readonly FlashcardsService service = new FlashcardsService();
+
+        public static void StartApp()
         {
             while (true)
             {
-                string menuText = "0 - Exit\n" +
-                    "1 - Manage stacks\n" +
-                    "2 - Manage flashcards\n" +
-                    "3 - Study\n" +
-                    "4 - View study session data";
-
-                var panel = new Panel(menuText);
-                panel.Padding = new Padding(1, 1, 1, 1);
-                AnsiConsole.Write(panel);
-                ProcessMenuInput();
+                DisplayMenu();
+                string option = Console.ReadLine();
+                ProcessMenuInput(option);
             }
         }
 
-        static public void ProcessMenuInput()
+        private static void DisplayMenu()
         {
-            
-            string option = Console.ReadLine();
+            string menuText = "0 - Exit\n" +
+                              "1 - Manage stacks\n" +
+                              "2 - Manage flashcards\n" +
+                              "3 - Study\n" +
+                              "4 - View study session data";
+
+            var panel = new Panel(menuText) { Padding = new Padding(1, 1, 1, 1) };
+            AnsiConsole.Write(panel);
+        }
+
+        private static void ProcessMenuInput(string option)
+        {
             switch (option)
             {
                 case "0":
-                    
+                    Environment.Exit(0); // Exits the application
                     break;
                 case "1":
                     ManageStacks();
                     break;
                 case "2":
-                    ManageFlashcards();
+                    
                     break;
                 case "3":
-
                     break;
                 case "4":
-
                     break;
                 default:
                     Console.WriteLine("Please enter a valid option!");
-                    ProcessMenuInput();
                     break;
             }
         }
 
-        static public void ManageStacks()
+        private static void ManageStacks()
         {
             Console.WriteLine("Input a stack name");
-            FlashcardsService service = new FlashcardsService();
+
             List<string> stackNames = service.GetStackNames();
-            var table = new Table();
-            table.AddColumn("Stack name");
-            foreach (string stackName in stackNames)
-            {
-                table.AddRow(stackName);
-            }
-            AnsiConsole.Write(table);
+            DisplayStackNames(stackNames);
+
             Console.WriteLine("Press 0 - to create a new stack");
             string option = Console.ReadLine();
+
             if (option == "0")
             {
-                Console.WriteLine("Please enter a new stack name");
-                string newStackName = Console.ReadLine();
-                while (stackNames.Contains(newStackName))
-                {
-                    Console.WriteLine("This name is already taken, enter a new one");
-                    newStackName = Console.ReadLine();
-                }
-                service.CreateStack(newStackName);
+                CreateNewStack(stackNames);
             }
             else if (stackNames.Contains(option))
             {
-                ManageFlashcards();
+                ManageFlashcards(option);
             }
             else
             {
                 Console.WriteLine("Please enter a valid stack name or 0 to create a new one");
-                
             }
-            ManageStacks();
         }
 
-        static public void ManageFlashcards()
+        private static void DisplayStackNames(List<string> stackNames)
         {
+            var table = new Table();
+            table.AddColumn("Stack name");
 
+            foreach (string stackName in stackNames)
+            {
+                table.AddRow(stackName);
+            }
+
+            AnsiConsole.Write(table);
+        }
+
+        private static void CreateNewStack(List<string> stackNames)
+        {
+            Console.WriteLine("Please enter a new stack name");
+            string newStackName = Console.ReadLine();
+
+            while (stackNames.Contains(newStackName))
+            {
+                Console.WriteLine("This name is already taken, enter a new one");
+                newStackName = Console.ReadLine();
+            }
+
+            service.CreateStack(newStackName);
+            Console.WriteLine("Stack created successfully.");
+        }
+
+        private static void ManageFlashcards(string currentStack)
+        {
+            while (true)
+            {
+                Console.WriteLine($"Current stack: {currentStack}");
+                string flashcardMenu = "0 - return to main menu\n" +
+                    "x - change current stack\n" +
+                    "v - view all flashcards in stack\n" +
+                    "a - view x amount of flashcards in stack\n" +
+                    "c - create a flashcard in current stack\n" +
+                    "e - edit a flashcard\n" +
+                    "d - delete a flashcard";
+
+                var panel = new Panel(flashcardMenu) { Padding = new Padding(1, 1, 1, 1) };
+                AnsiConsole.Write(panel);
+                ProcessFlashcardsMenuInput(Console.ReadLine(), currentStack);   
+            }
+            
+        }
+
+        private static void ProcessFlashcardsMenuInput(string option, string currentStack)
+        {
+            switch (option)
+            {
+                case "0":
+                    StartApp(); // Exits the application
+                    break;
+                case "x":
+                    ManageStacks();
+                    break;
+                case "v":
+                    ViewFlashcards(null, currentStack);
+                    break;
+                case "a":
+                    ViewNumberOfFlashcards(currentStack);
+                    break;
+                case "c":
+                    AddNewFlashcard(currentStack);
+                    break;
+                case "e":
+                    EditFlashcard(currentStack);
+                    break;
+                case "d":
+                    DeleteFlashcard(currentStack);
+                    break;
+                default:
+                    Console.WriteLine("Please enter a valid option!");
+                    break;
+            }
+        }
+
+        private static void ViewFlashcards(int? amount, string currentStack)
+        {
+            List<FlashcardDTO> flashcards = service.GetFlashcards(amount, currentStack);
+            DisplayFlashcards(flashcards);
+        }
+
+        private static void DisplayFlashcards(List<FlashcardDTO> flashcards)
+        {
+            var table = new Table();
+            table.AddColumn("Id");
+            table.AddColumn("Front");
+            table.AddColumn("Back");
+
+            foreach (FlashcardDTO flashcard in flashcards)
+            {
+                table.AddRow(flashcard.FlashcardId.ToString(), flashcard.Front, flashcard.Back);
+            }
+            
+
+            AnsiConsole.Write(table);
+        }
+
+        private static void AddNewFlashcard(string currentStack)
+        {
+            Console.WriteLine("Please enter the front of the flashcard");
+            string front = Console.ReadLine();
+            Console.WriteLine("Please enter the back of the flashcard");
+            string back = Console.ReadLine();
+            service.AddFlashcard(front, back, currentStack);
+        }
+
+        private static void ViewNumberOfFlashcards(string currentStack)
+        {
+            Console.WriteLine("Please enter the number of flashcards to be viewed");
+            while (true)
+            {
+                try
+                {
+                    string input = Console.ReadLine();
+                    int rows = int.Parse(input);
+                    ViewFlashcards(rows, currentStack);
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Invalid number");
+                }
+            }
+        }
+
+        private static void EditFlashcard(string currentStack)
+        {
+            Console.WriteLine("Please enter the id of the flashcard");
+            string id = Console.ReadLine();
+            Console.WriteLine("Please enter the front of the flashcard");
+            string front = Console.ReadLine();
+            Console.WriteLine("Please enter the back of the flashcard");
+            string back = Console.ReadLine();
+            
+            service.EditFlashcard(front, back, id);
+        }
+
+        private static void DeleteFlashcard(string currentStack)
+        {
+            Console.WriteLine("Please enter the id of the flashcard to be deleted");
+            while (true)
+            {
+                try
+                {
+                    string input = Console.ReadLine();
+                    int id = int.Parse(input);
+                    DeleteFlash(id, currentStack);
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Invalid number");
+                }
+            }
         }
     }
 }
